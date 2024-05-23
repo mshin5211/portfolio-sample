6ad172d888fa07e3d0cad333307d370e0cc5ef27
mshin5211 <130117063+mshin5211@users.noreply.github.com>
2024-05-23 10:38:35 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDNvaUaNKwsy99pNb_b7z4X6hg7t5TQyDA",
-  authDomain: "portfolio-sample-mshin.firebaseapp.com",
-  projectId: "portfolio-sample-mshin",
-  storageBucket: "portfolio-sample-mshin.appspot.com",
-  messagingSenderId: "6367319430",
-  appId: "1:6367319430:web:214887a0d4275a41ec8807",
-  measurementId: "G-2G7X9LVG5L" };
-
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const inter = Inter({ subsets: ["latin"] });
 
